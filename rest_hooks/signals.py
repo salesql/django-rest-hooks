87906acb518b06f043e13a87f0a3df50c47f7fe0
Miguel Baquedano <miguel.baquedano@salesql.com>
2024-04-25 10:30:53 +0200

    Support Django >= 3.0

--- rest_hooks/signals.py
+++ rest_hooks/signals.py
@@ -1,6 +1,12 @@
+import django
 from django.dispatch import Signal
 
 
-hook_event = Signal(providing_args=['action', 'instance'])
-raw_hook_event = Signal(providing_args=['event_name', 'payload', 'user'])
-hook_sent_event = Signal(providing_args=['payload', 'instance', 'hook'])
+if django.VERSION >= (3, 0):
+    hook_event = Signal()
+    raw_hook_event = Signal()
+    hook_sent_event = Signal()
+else:
+    hook_event = Signal(providing_args=['action', 'instance'])
+    raw_hook_event = Signal(providing_args=['event_name', 'payload', 'user'])
+    hook_sent_event = Signal(providing_args=['payload', 'instance', 'hook'])
