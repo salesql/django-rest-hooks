08e74f0ffdf429c6a22dafdff0c54ddb871c4d5f
Miguel Baquedano <miguel.baquedano@salesql.com>
2024-04-29 16:01:29 +0200

    Change to default Autofield to prevent migratiosn

--- rest_hooks/apps.py
+++ rest_hooks/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RestHooksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
     name = 'rest_hooks'
