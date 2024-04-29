c1a5840ddc3c1c270f2c21093ad6d773736ee3e2
Miguel Baquedano <miguel.baquedano@salesql.com>
2024-04-29 16:29:30 +0200

    Add migration

---
+++ rest_hooks/migrations/0003_alter_hook_user.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('rest_hooks', '0002_swappable_hook_model'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='hook',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='%(class)ss', to=settings.AUTH_USER_MODEL),
+        ),
+    ]
